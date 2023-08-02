8c9c0efba0c4a46766340fa46e6fcbb9edfb5f3e
impaban <subpawan055@gmail.com>
2023-08-02 13:51:43 +0545

    jwt

--- routes/posts.js
+++ routes/posts.js
@@ -3,73 +3,8 @@ const users = require('../models/user');
 const post = require('../models/post');
 const bcrypt = require('bcrypt');
 
-/*/ create post
-router.post('/:id', async (req, res) => {
-    const newPost=new post(req.body);
-    try{
-     const savepost = await newPost.save();
-     res.sendStatus(200).json(savepost);
-    }catch(err){
-        res.status(500).json(err);
-    }
-});
 
-// update post
-router.put("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.username === req.body.username) {
-        try {
-          const updatedPost = await Post.findByIdAndUpdate(
-            req.params.id,
-            {
-              $set: req.body,
-            },
-            { new: true }
-          );
-          res.status(200).json(updatedPost);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(401).json("You can update only your post!");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-    
-});
-//delete post 
-router.delete("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.username === req.body.username) {
-        try {
-          await post.delete();
-          res.status(200).json("Post has been deleted...");
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(401).json("You can delete only your post!");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
 
-//get post
-router.get("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-module.exports = router;
-*/
 // create post (protected route - requires JWT authentication)
 router.post('/:id', validateToken, async (req, res) => {
   const newPost = new post(req.body);
@@ -81,7 +16,7 @@ router.post('/:id', validateToken, async (req, res) => {
   }
 });
 
-// update post (protected route - requires JWT authentication)
+// update post 
 router.put("/:id", validateToken, async (req, res) => {
   try {
       const post = await Post.findById(req.params.id);
@@ -106,7 +41,7 @@ router.put("/:id", validateToken, async (req, res) => {
   }
 });
 
-// delete post (protected route - requires JWT authentication)
+// delete post 
 router.delete("/:id", validateToken, async (req, res) => {
   try {
       const post = await Post.findById(req.params.id);
--- routes/users.js
+++ routes/users.js
@@ -5,66 +5,10 @@ const post = require('../models/post');
 const bcrypt = require('bcrypt');
 const validateToken = require('./validateto');
 
-/*// Update
-router.put('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        if (req.body.password) {
-            const salt = await bcrypt.genSalt(12);
-            req.body.password = await bcrypt.hash(req.body.password, salt);
-        }
-        try {
-            const updatedUser = await users.findByIdAndUpdate(
-                req.params.id,
-                {
-                    $set: req.body,
-                },
-                { new: true }
-            );
 
-            res.status(200).json(updatedUser);
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    } else {
-        res.status(401).json('You can only update your account');
-    }
-});
+const validateToken = require('./'); 
 
-// Delete
-router.delete('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        try {
-            const user = await users.findById(req.params.id);
-            try {
-                await post.deleteMany({ username: user.username });
-                await users.findByIdAndDelete(req.params.id);
-                res.status(200).json('User has been deleted');
-            } catch (err) {
-                res.status(404).json('User not found');
-            }
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    } else {
-        res.status(401).json('You can only delete your account');
-    }
-});
-//get post
-router.get('/:id', async (req, res) => {
-    try {
-        const foundUser = await users.findById(req.params.id);
-        const { password, ...others } = foundUser._doc;
-        res.status(200).json(others);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-module.exports = router;
-*/
-const validateToken = require('./'); // Update the path to the middleware
-
-// Update (protected route - requires JWT authentication)
+// Update
 router.put('/:id', validateToken, async (req, res) => {
     if (req.body.userId === req.params.id || req.user.userId === req.params.id) {
         if (req.body.password) {
@@ -89,7 +33,7 @@ router.put('/:id', validateToken, async (req, res) => {
     }
 });
 
-// Delete (protected route - requires JWT authentication)
+// Delete 
 router.delete('/:id', validateToken, async (req, res) => {
     if (req.body.userId === req.params.id || req.user.userId === req.params.id) {
         try {
@@ -109,7 +53,7 @@ router.delete('/:id', validateToken, async (req, res) => {
     }
 });
 
-//get post (protected route - requires JWT authentication)
+//get post 
 router.get('/:id', validateToken, async (req, res) => {
     try {
         if (req.user.userId === req.params.id) {
@@ -123,4 +67,5 @@ router.get('/:id', validateToken, async (req, res) => {
         res.status(500).json(err);
     }
 });
+module.exports = router;
 
